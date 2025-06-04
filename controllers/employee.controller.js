c501b323a530595be3048619e0a5725060c82af9
sinpell <catty12032@gmail.com>
2025-06-04 16:36:53 +0700

    edit employee

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -1,15 +1,36 @@
 const employeeSchema = require('../models/employee.model');
 
 exports.getEmployees = async (req, res) => {
-    try {
-        const employees = await employeeSchema.find({}, 'firstname lastname department position phone').sort({ firstname: 1 });
-        
-        return res.status(200).json({
-            success: true,
-            data: employees
-        });
-    } catch (error) {
-        console.error('Error fetching employees:', error);
-        return res.status(500).json({ message: 'Internal server error' });
-    }
-}
+  try {
+    const employees = await employeeSchema.aggregate([
+      {
+        $addFields: {
+          numericId: {
+            $toInt: { $substr: ['$_id', 3, -1] } // ตัด "EMP" ออกแล้วแปลงเป็นเลข
+          }
+        }
+      },
+      {
+        $sort: { numericId: 1 } // เรียงตามเลขท้ายจริง ๆ
+      },
+      {
+        $project: {
+          _id: 1,
+          firstname: 1,
+          lastname: 1,
+          department: 1,
+          position: 1,
+          phone: 1
+        }
+      }
+    ]);
+
+    return res.status(200).json({
+      success: true,
+      data: employees
+    });
+  } catch (error) {
+    console.error('Error fetching employees:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
