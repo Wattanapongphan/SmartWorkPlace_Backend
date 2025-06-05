8f3e7c95a66c79d907ec81a064b6e77e2890a9ad
Wattanapongphan <wattanapongphan@gmail.com>
2025-06-05 14:34:37 +0700

    edit api floor

--- controllers/floor.controller.js
+++ controllers/floor.controller.js
@@ -1,17 +1,17 @@
 exports.getFloors = async (req, res) => {
   try {
     const allFloors = [
-      { id: "1", name: "ชั้น 3", floor: "cnx-1" },
-      { id: "2", name: "ชั้น 4", floor: "cnx-1" },
-      { id: "3", name: "ชั้น 1", floor: "cnx-2" },
-      { id: "4", name: "ชั้น 1", floor: "bkk-1" },
-      { id: "5", name: "ชั้น 2", floor: "bkk-2" },
-      { id: "6", name: "ชั้น 1", floor: "kkc-1" },
-      { id: "7", name: "ชั้น 2", floor: "kkc-2" },
-      { id: "8", name: "ชั้น 1", floor: "hdy-1" },
-      { id: "9", name: "ชั้น 2", floor: "hdy-2" },
-      { id: "10", name: "ชั้น 1", floor: "sri-1" },
-      { id: "11",name: "ชั้น 2", floor: "sri-2" },
+      { id: "cnx-1-1", name: "ชั้น 3", floor: "cnx-1" },
+      { id: "cnx-1-2", name: "ชั้น 4", floor: "cnx-1" },
+      { id: "cnx-2-1", name: "ชั้น 1", floor: "cnx-2" },
+      { id: "bkk-1-1", name: "ชั้น 1", floor: "bkk-1" },
+      { id: "bkk-1-2", name: "ชั้น 2", floor: "bkk-2" },
+      { id: "kkc-1-1", name: "ชั้น 1", floor: "kkc-1" },
+      { id: "kkc-1-2", name: "ชั้น 2", floor: "kkc-2" },
+      { id: "hdy-1-1", name: "ชั้น 1", floor: "hdy-1" },
+      { id: "hdy-1-2", name: "ชั้น 2", floor: "hdy-2" },
+      { id: "sri-1-1", name: "ชั้น 1", floor: "sri-1" },
+      { id: "sri-1-2",name: "ชั้น 2", floor: "sri-2" },
     ];
 
     const { floor } = req.query;
@@ -28,7 +28,7 @@ exports.getFloors = async (req, res) => {
 
       return res.status(200).json({
        message:`Get floor ${floor} Successfully`,
-        data: filteredfloors.map((f) => f.name),
+        data: filteredfloors.map((f) => ({ id: f.id, name: f.name })),
       });
     }
 
